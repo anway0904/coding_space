b61896461a6297c3feae142541b7cbf3ff349ef1
Anway Shirgaonkar <shirgaonkar.a@northeastern.edu>
2024-12-20 15:57:52 -0500

    added two sum sorted

---
+++ cpp/neetcode/two_pointers/two_sum_sorted.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+/*
+Given an array of integers numbers that is sorted in non-decreasing order.
+Return the indices (1-indexed) of two numbers, [index1, index2], such that they add up 
+to a given target number target and index1 < index2. Note that index1 and index2 cannot be equal, therefore you may not use the same element twice.
+
+There will always be exactly one valid solution.
+
+Your solution must use O(1) additional space.
+
+Example 1:
+Input: numbers = [1,2,3,4], target = 3
+Output: [1,2]
+
+Explanation:
+The sum of 1 and 2 is 3. Since we are assuming a 1-indexed array, index1 = 1, index2 = 2. We return [1, 2].
+*/
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& numbers, int target) {
+        std::vector<int>::iterator sPtr = numbers.begin();
+        std::vector<int>::iterator ePtr = numbers.end() -1;
+
+        while (*sPtr + *ePtr != target) {
+
+            while (*sPtr + *ePtr > target){
+                ePtr--;
+            }
+
+            while (*sPtr + *ePtr < target){
+                sPtr++;
+            }
+        }
+
+        return {static_cast<int>(sPtr - numbers.begin() + 1), 
+                static_cast<int>(ePtr - numbers.begin() + 1)};
+    }
+};
