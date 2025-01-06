07aedee4b110ab1d1068e21ed77ae0324b5172d7
Anway Shirgaonkar <shirgaonkar.a@northeastern.edu>
2025-01-05 20:50:35 -0500

    added cavas test

---
+++ cpp/miscellaneous/canvas_test.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+int main() {
+    const int WIDTH = 500;
+    const int HEIGHT = 500;
+    
+    std::cout << "\033[2J"; // Clear the screen
+    std::cout << "\033[H";  // Move cursor to home position (top left)
+
+    
+    return 0;
+}
